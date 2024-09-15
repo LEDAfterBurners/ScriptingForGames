6cbb5efa2ed51852c5e168bb9b380b8a1d233875
Joey Nilsson <jwnilsson010@gmail.com>
2024-09-15 13:45:37 -0600

    added ash baby, changed TransformController
    i dont know how I made it move like this but it's funny asf

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,18 @@ public class TransformController : MonoBehaviour
     public void Update()
     {
         // Move the target GameObject
-        var x = Mathf.PingPong(Time.time,3);
-        var p = new Vector3(0,x,0);
-        transform.position = p;
+        int radius = 1;
+        int degrees = 1;
+        double circlePosX = Math.Cos(degrees) * radius;
+        double circlePosY = Math.Sin(degrees) * radius;
+        var p = new Vector3(Time.time*(float)circlePosX,Time.time*(float)circlePosY, 0);
+        transform.Translate(p);
+        /*var x = Mathf.PingPong(Time.time,6);
+        var y = Mathf.PingPong(Time.time,1);
+        var p = new Vector3(x,Math.Cos((float)y),0);
+        transform.position = p;*/
         
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0,30,0)*Time.deltaTime);
+        transform.Rotate(new Vector3(360,0,0)*Time.deltaTime);
     }
 }
