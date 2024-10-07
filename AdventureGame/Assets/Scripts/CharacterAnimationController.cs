dc28ab0576e348a9d9b9572bfda1d814e4b40b0a
Joey Nilsson <jwnilsson010@gmail.com>
2024-10-06 20:21:30 -0600

    Created animator and animations

---
+++ AdventureGame/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private void Start()
+    {
+        // Cache the Animator component attached to CharacterArt
+        animator = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        // Handle running and idling
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetTrigger("RunTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+        
+        // Handle jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("DoubleJumpTrigger");
+        }
+        
+        // Handle wall jumping
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("WallJumpTrigger");
+        }
+        
+        // Handle falling
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("FallTrigger");
+        }
+        
+        // Handle hits
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("HitTrigger");
+        }
+    }
+}
